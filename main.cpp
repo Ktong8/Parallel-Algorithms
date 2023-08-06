3b9ab78183f22692110fa875afb84f01adc7c327
Kevin Tong <kevintong@Kevins-MacBook-Pro.local>
2023-08-06 13:04:18 -0700

    Tested the parallel sum functions and added benchmark functions for them

--- main.cpp
+++ main.cpp
@@ -1,42 +1,74 @@
 #include "parallel_prefix_sum.h"
 #include "timer.h"
+#include "par_sum_test.h"
 #include <iostream>
 
 constexpr int VECTOR_LEN = 100000;
 constexpr int ARRAY_LEN = 1000;
 
-long long prefix_sum_loop(std::vector<int>& arr) {
-    long long sum = 0ll;
+int prefix_sum_loop(std::vector<int>& arr) {
+    int sum = 0;
     for (int i = 0; i < arr.size(); ++i) {
         sum += arr[i];
     }
     return sum;
 }
 
-int main() {
+void TestFunctions() {
+    TestVectorSum();
+    TestArraySum();
+    TestVectorArrSum();
+}
+
+
+void BenchmarkVector(std::string message, size_t size, std::function<int(std::vector<int>&)> func) {
     using benchmark::Timer;
-    std::vector<int> a(VECTOR_LEN);
-    std::array<int, ARRAY_LEN> b{{}};
-    for (int i = 0; i < VECTOR_LEN; ++i) {
+    std::vector<int> a(size);
+    for (size_t i = 0; i < size; ++i) {
         a[i] = i;
     }
-    for (int i = 0; i < ARRAY_LEN; ++i) {
-        b[i] = i;
+    {
+        Timer timer {message};
+        func(a);
+    }
+}
+
+template <size_t N>
+void BenchmarkArray(std::string message, std::function<int(std::array<int, N>&)> func) {
+    using benchmark::Timer;
+    std::array<int, N> a{};
+    for (size_t i = 0; i < N; ++i) {
+        a[i] = i;
     }
     {
-        std::string message = "Vector with " + std::to_string(VECTOR_LEN) + " integers";
-        Timer timer = Timer(message);
-        prefix_sums::prefix_sum(a);
+        Timer timer{message};
+        func(a);
     }
-    {
-        std::string message = "Array with " + std::to_string(ARRAY_LEN) + " integers";
-        Timer timer = Timer(message);
-        prefix_sums::prefix_sum(b);
-    }
-    {
-        std::string message = "Vector with " + std::to_string(VECTOR_LEN) + " integers regular";
-        Timer timer = Timer(message);
-        prefix_sums::prefix_sum(a);
-    }
+}
+
+int main() {
+    using benchmark::Timer;
+    TestFunctions();
+    std::vector<int> a(VECTOR_LEN);
+    std::array<int, ARRAY_LEN> b{{}};
+    BenchmarkVector(
+        "Vector with " + std::to_string(VECTOR_LEN) + " integers",
+        VECTOR_LEN,
+        prefix_sums::prefix_sum<int>
+    );
+    BenchmarkArray<ARRAY_LEN>(
+        "Array with " + std::to_string(ARRAY_LEN) + " integers",
+        prefix_sums::prefix_sum<int, ARRAY_LEN>
+    );
+    BenchmarkVector(
+        "Vector with " + std::to_string(VECTOR_LEN) + " integers with for loop",
+        VECTOR_LEN,
+        prefix_sum_loop
+    );
+    BenchmarkVector(
+        "Vector with " + std::to_string(VECTOR_LEN) + " integers cache friendly",
+        VECTOR_LEN,
+        prefix_sums::prefix_sum_arr<int>
+    );
     return 0;
 }
